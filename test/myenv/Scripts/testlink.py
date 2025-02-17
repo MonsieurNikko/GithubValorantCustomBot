06cab7629d3735656f136f91575d991ed8455bf2
HUYNH Duc Duy <151777338+MonsieurNikko@users.noreply.github.com>
2025-02-17 20:43:02 +0100

    Delete test/myenv directory

--- test/myenv/Scripts/testlink.py
+++
@@ -1,7 +0,0 @@
-import socket
-
-try:
-    ip = socket.gethostbyname("www.google.com")
-    print("www.google.com résolu en:", ip)
-except Exception as e:
-    print("Erreur lors de la résolution:", e)
